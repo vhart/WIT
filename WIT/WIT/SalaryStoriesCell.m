7e7354d6a7ea0ff31ab898fe28b97b184b5cf773
Diana <diana_elezaj@hotmail.com>
2015-10-31 15:48:50 -0400

    Added Salary Cell files

---
+++ WIT/WIT/SalaryStoriesCell.m
@@ -0,0 +1,23 @@
+//
+//  SalaryStoriesCell.m
+//  WIT
+//
+//  Created by Diana Elezaj on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import "SalaryStoriesCell.h"
+
+@implementation SalaryStoriesCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
