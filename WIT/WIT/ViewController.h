39e991e6d392569f4e4fa6ef371a9ec13337298f
Varindra <varindrahart@gmail.com>
2015-10-31 12:25:28 -0400

    pods installed

---
+++ WIT/WIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WIT
+//
+//  Created by Varindra Hart on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WIT/WIT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WIT
+//
+//  Created by Varindra Hart on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
