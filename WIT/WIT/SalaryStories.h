afe0d39911835df71a480a98a216897c3b268ac9
Diana <diana_elezaj@hotmail.com>
2015-10-31 13:17:10 -0400

    Added Salary section

---
+++ WIT/WIT/SalaryStories.h
@@ -0,0 +1,13 @@
+//
+//  SalaryStories.h
+//  WIT
+//
+//  Created by Diana Elezaj on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SalaryStories : UITableViewController
+
+@end
