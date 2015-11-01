1a9fe912db8030a46d95359c89dfd90ec92bad85
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-10-31 22:39:52 -0400

    Post Section

--- WIT/WIT/TextPostCreationViewController.m
+++ WIT/WIT/TextPostCreationViewController.m
@@ -8,10 +8,42 @@
 
 #import "TextPostCreationViewController.h"
 
-@implementation TextPostCreationViewController <UITextViewDelegate>
+@interface TextPostCreationViewController () <UITextViewDelegate>
 
-@property (nonatomic, weak) IBOutlet UITextView *textView;
-@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
-@property (nonatomic, weak) IBOutlet UILabel *tagLabel;
+
+@property (nonatomic, weak) IBOutlet UITextField *titleTextField;
+@property (nonatomic, weak) IBOutlet UITextView *inputTextView;
+@property (nonatomic, weak) IBOutlet UITextField *tagTextField;
+@property (weak, nonatomic) IBOutlet UIButton *saveButtonTapped;
+@property (weak, nonatomic) IBOutlet UIView *saveButtonView;
+@end
+
+
+@implementation TextPostCreationViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.titleTextField.layer.cornerRadius = 10;
+    self.inputTextView.layer.cornerRadius = 10;
+    self.tagTextField.layer.cornerRadius = 10;
+
+
+    
+    self.saveButtonView.layer.cornerRadius = 25;
+    self.saveButtonTapped.layer.cornerRadius = 20;
+    
+    
+//    self.inputTextView.text = self.note.text;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+   
+}
 
 @end
