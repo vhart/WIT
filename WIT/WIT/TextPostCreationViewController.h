6a5e4f5d1425a4ca91ff28cfc6f5a9b1035ef50c
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-10-31 21:46:25 -0400

    Commit

---
+++ WIT/WIT/TextPostCreationViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextPostCreationViewController.h
+//  WIT
+//
+//  Created by Chris David on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextPostCreationViewController : UIViewController
+
+@end
---
+++ WIT/WIT/TextPostCreationViewController.m
@@ -0,0 +1,17 @@
+//
+//  TextPostCreationViewController.m
+//  WIT
+//
+//  Created by Chris David on 10/31/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import "TextPostCreationViewController.h"
+
+@implementation TextPostCreationViewController <UITextViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextView *textView;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) IBOutlet UILabel *tagLabel;
+
+@end
